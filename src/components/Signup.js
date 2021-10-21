1f562603c5da8364baa938642f03badf334b0870
akshay <cakshay001@gmail.com>
2021-10-21 16:20:06 +0530

    added routes

---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigation } from './navigation'
+
+export default function Signup() {
+    return (
+      <>
+      <Navigation/>
+     <h2>hellp</h2>
+     </>
+    )
+}
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -3,7 +3,8 @@ export const Features = (props) => {
     <div id='features' className='text-center'>
       <div className='container'>
         <div className='col-md-10 col-md-offset-1 section-title'>
-          <h2>Features</h2>
+          <h2 style={{paddingTop:"5rem"}}>Our website will help you answer following questions:
+</h2>
         </div>
         <div className='row'>
           {props.data
