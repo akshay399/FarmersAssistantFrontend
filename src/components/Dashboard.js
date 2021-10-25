22b6a5aaa5e61d2e544b9fafaed557285bdf387a
akshay <cakshay001@gmail.com>
2021-10-25 11:48:04 +0530

    fixed dashboard and getdata button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,18 +9,28 @@ import { Header } from './header';
 import { Navigation } from './navigation';
   import newsStyles from './newsStyles'
   import useStyles from './newsStyles';
+  import JsonData from '../data/data.json'
+import { Features } from './features';
+import { About } from './about';
+  
 
   function Dashboard(props) {
+    const [landingPageData, setLandingPageData] = useState({});
     const classes = useStyles();
     const {handleLogout, user} = props;
   console.log("user in dashboard", user);
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
 
     return (
         <>
          {/* <div className={classes.toolbar} /> */}
             {/* <div className={classes.toolbar} /> */}
             {/* <div className={classes.toolbar} /> */}
-       <Header />
+       <Header user={user} />
+       <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
         </>
     )
 }
